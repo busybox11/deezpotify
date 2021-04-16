53f873711570cc2d18cdee3543cf764b62035ca8
busybox <planetgeek11@gmail.com>
2021-04-16 19:08:16 +0200

    Added navbar color animation on scroll

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -1,11 +1,24 @@
-document.addEventListener('DOMContentLoaded', init, false);
+let topnav = document.getElementById('topnav')
+
+document.addEventListener('DOMContentLoaded', init, false)
+
 function init() {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.register('/js/service-worker.js')
 		.then((reg) => {
-			console.log('Service worker registered -->', reg);
+			console.log('Service worker registered -->', reg)
 		}, (err) => {
-			console.error('Service worker not registered -->', err);
-		});
+			console.error('Service worker not registered -->', err)
+		})
+	}
+}
+
+document.onscroll = function(data) {
+	if (window.pageYOffset > 100) {
+		topnav.classList.add('scrolled')
+	} else {
+		try {
+			topnav.classList.remove('scrolled')
+		} catch(e) {}
 	}
 }
