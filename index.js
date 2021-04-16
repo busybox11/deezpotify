6da0d5cfcb0417d27cc784ff25f480d0560c04c8
busybox <planetgeek11@gmail.com>
2021-04-16 18:12:27 +0200

    Added app

--- index.js
+++ index.js
@@ -3,25 +3,45 @@ const path = require('path')
 const fs = require('fs')
 const https = require('https')
 
+let spotifyModule = require('./spotify')
+
 const httpPort = 80
-const httpsPort = 443
-const key = fs.readFileSync('./certs/localhost.key');
-const cert = fs.readFileSync('./certs/localhost.crt');
+const httpsPort = 44100
+const key = fs.readFileSync('./certs/localhost.key')
+const cert = fs.readFileSync('./certs/localhost.crt')
 
 const app = express()
-const server = https.createServer({key: key, cert: cert }, app);
+const server = https.createServer({key: key, cert: cert }, app)
 
 app.use((req, res, next) => {
 	if (!req.secure) {
-		return res.redirect('https://' + req.headers.host + req.url);
+		return res.redirect('https://' + req.headers.host + req.url)
 	}
-	next();
+	next()
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+app.get('/app', function(req, res) {
+	res.sendFile(path.join(__dirname, 'views/app.html'))
+})
+
+app.get('/auth/spotify', function(req, res) {
+	if (req.param('code') == undefined) {
+		res.redirect(spotifyModule.getLoginURL())
+	} else {
+		spotifyModule.getTokens(req.param('code')).then(function(data) {
+			res.cookie('refresh', data.refresh)
+			res.cookie('token', data.token)
+			res.cookie('time', Date.now() + data.expires)
+
+			res.redirect('/app')
+		})
+	}
 })
 
 app.listen(httpPort, function () {
---
+++ spotify.js
@@ -0,0 +1,43 @@
+let SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config()
+
+let scopes = ['user-read-private', 'user-read-email'],
+	state = 'login';
+
+function getLoginURL() {
+	let spotifyApi = new SpotifyWebApi({
+		redirectUri: process.env.SPOTIFY_URI_DEV,
+		clientId: process.env.SPOTIFY_CLIENT_ID
+	})
+
+	return spotifyApi.createAuthorizeURL(scopes, state)
+}
+
+function getTokens(code) {
+	return new Promise((resolve, reject) => {
+		let spotifyApi = new SpotifyWebApi({
+			clientId: process.env.SPOTIFY_CLIENT_ID,
+			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+			redirectUri: process.env.SPOTIFY_URI_DEV
+		})
+
+		spotifyApi.authorizationCodeGrant(code).then(function(data) {
+			console.log('The token expires in ' + data.body['expires_in'])
+			console.log('The access token is ' + data.body['access_token'])
+			console.log('The refresh token is ' + data.body['refresh_token'])
+
+			return resolve({
+				'expires': data.body['expires_in'],
+				'token': data.body['access_token'],
+				'refresh': data.body['refresh_token']
+			})
+		},	function(err) {
+			return reject('Something went wrong!', err);
+		})
+	})
+}
+
+module.exports = {
+	getLoginURL,
+	getTokens
+}
