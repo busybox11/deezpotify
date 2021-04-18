df7d578605b2cd91000843ae3b174a1be5335713
busybox <planetgeek11@gmail.com>
2021-04-18 02:17:07 +0200

    Even better playing layout!

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -1,6 +1,10 @@
+let body = document.querySelector('body')
 let app = document.getElementById('app')
+let player = document.getElementById('player')
 let topNav = document.getElementById('topnav')
 let bottomNavs = document.getElementById('bottom-navs')
+
+let scrollPos;
 
 document.addEventListener('DOMContentLoaded', init, false)
 
@@ -26,13 +30,24 @@ document.onscroll = function(data) {
 }
 
 function openPlayingTrack() {
+	scroll = body.scrollTop
+	body.scrollTop = 0
+	body.style.overflow = "hidden"
 	app.style.opacity = "0"
+	app.style.zIndex = "-2"
+	player.style.opacity = "1"
+	player.style.zIndex = "2"
 	bottomNavs.style.bottom = "-10rem"
 	document.querySelector('#topnav-left-icon').outerHTML = '<i id="topnav-left-icon" class="iconify navbar-icons navbar-menu-icon" data-icon="mdi-chevron-down" onclick="closePlayingTrack()"></i>'
 }
 
 function closePlayingTrack() {
+	body.scrollTop = scroll
+	body.style.overflow = "auto"
 	app.style.opacity = "1"
+	app.style.zIndex = "2"
+	player.style.opacity = "0"
+	player.style.zIndex = "-2"
 	bottomNavs.style.bottom = "0"
 	document.querySelector('#topnav-left-icon').outerHTML = '<i id="topnav-left-icon" class="iconify navbar-icons navbar-menu-icon" data-icon="mdi-menu"></i>'
 }
